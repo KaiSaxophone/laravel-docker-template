4571f27cfceb32b67acc670181f0e439448ee953
KaiSaxophone <kai.saxophone@gmail.com>
2026-05-18 17:23:01 +0900

    add: Modelを介したレコード全件取得処理の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 // /todoアクセス時の処理
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+
         return view('todo.index');
     }
 }
