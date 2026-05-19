8f670baf76a15e277b505925fb899d50022fcb0b
KaiSaxophone <kai.saxophone@gmail.com>
2026-05-19 17:45:01 +0900

    refactor: フォーム入力値を一括代入に修正

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -23,15 +23,14 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    // フォームに入力されたcontentを取得
+    // フォームに入力された値を取得
     public function store(Request $request)
     {
-        $content = $request->input('content');
-        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        // フォームから送信された入力値を一括取得
+        $inputs = $request->all();
+
         $todo = new Todo();
-        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->content = $content;
-        // 3. Todoインスタンスの->save()を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->fill($inputs);
         $todo->save();
 
         return redirect()->route('todo.index');
--- src/app/Todo.php
+++ src/app/Todo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     protected $table = 'todos';
+
+    // フォーム入力値の項目指定
+    protected $fillable = [
+        'content',
+    ];
 }
